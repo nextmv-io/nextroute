3dfd45fd0fdd6188ea95fb6703926957a4027401
David Rijsman <drijsman@nextmv.io>
2024-05-07 08:25:41 +0200

    Interleave constraint, buffer planned units

--- model_constraint_interleave.go
+++ model_constraint_interleave.go
@@ -5,6 +5,7 @@ package nextroute
 import (
 	"fmt"
 	"github.com/nextmv-io/nextroute/common"
+	"math"
 )
 
 // InterleaveConstraint is a constraint that disallows certain target to be
@@ -24,6 +25,10 @@ type InterleaveConstraint interface {
 	// TargetDisallowedInterleaves returns the disallowed interleaves for the
 	// given target.
 	TargetDisallowedInterleaves(target ModelPlanUnit) []DisallowedInterleave
+
+	// UpdateConstraintStopData is called when a stop has been added to a
+	// solution for each stop in a vehicle after the added stop.
+	UpdateConstraintStopData(s SolutionStop) (Copier, error)
 }
 
 // NewInterleaveConstraint returns a new InterleaveConstraint.
@@ -78,6 +83,87 @@ type interleaveConstraintImpl struct {
 	disallowedInterleaves       []DisallowedInterleave
 	sourceDisallowedInterleaves map[ModelPlanUnit][]DisallowedInterleave
 	targetDisallowedInterleaves map[ModelPlanUnit][]DisallowedInterleave
+}
+
+type interleaveSolutionStopSpan struct {
+	first int
+	last  int
+}
+
+type interleaveConstraintData struct {
+	solutionPlanStopUnits map[int]interleaveSolutionStopSpan
+}
+
+func (i *interleaveConstraintData) Copy() Copier {
+	solutionPlanStopUnits := make(map[int]interleaveSolutionStopSpan, len(i.solutionPlanStopUnits))
+
+	for k := range i.solutionPlanStopUnits {
+		solutionPlanStopUnits[k] = interleaveSolutionStopSpan{
+			first: i.solutionPlanStopUnits[k].first,
+			last:  i.solutionPlanStopUnits[k].last,
+		}
+	}
+
+	return &interleaveConstraintData{
+		solutionPlanStopUnits: solutionPlanStopUnits,
+	}
+}
+
+func (l *interleaveConstraintImpl) UpdateConstraintStopData(s SolutionStop) (Copier, error) {
+	if s.IsLast() {
+		data := interleaveConstraintData{
+			solutionPlanStopUnits: make(map[int]interleaveSolutionStopSpan),
+		}
+
+		modelPlanStopsUnits := map[SolutionPlanStopsUnit]struct{}{}
+		stop := s.Vehicle().First().Next()
+		for !stop.IsLast() {
+
+			planStopsUnit := stop.PlanStopsUnit()
+
+			stop = stop.Next()
+
+			if _, ok := modelPlanStopsUnits[planStopsUnit]; ok {
+				continue
+			}
+
+			if modelPlanUnitsUnit, ok := planStopsUnit.ModelPlanStopsUnit().PlanUnitsUnit(); ok {
+				modelPlanStopsUnits[planStopsUnit] = struct{}{}
+
+				planUnitsUnitFirst := math.MaxInt64
+				planUnitsUnitLast := math.MinInt64
+
+				for _, planUnit := range modelPlanUnitsUnit.PlanUnits() {
+					if modelPlanStopsUnit, ok := planUnit.(ModelPlanStopsUnit); ok {
+						solutionPlanStopsUnit := s.Solution().SolutionPlanStopsUnit(modelPlanStopsUnit).(SolutionPlanStopsUnit)
+						if solutionPlanStopsUnit.IsPlanned() {
+							first := solutionPlanStopsUnit.SolutionStops()[0].Position()
+							last := solutionPlanStopsUnit.SolutionStops()[len(solutionPlanStopsUnit.SolutionStops())-1].Position()
+
+							data.solutionPlanStopUnits[modelPlanStopsUnit.Index()] = interleaveSolutionStopSpan{
+								first: first,
+								last:  last,
+							}
+
+							if first < planUnitsUnitFirst {
+								planUnitsUnitFirst = first
+							}
+							if last > planUnitsUnitLast {
+								planUnitsUnitLast = last
+							}
+						}
+					}
+				}
+				data.solutionPlanStopUnits[modelPlanUnitsUnit.Index()] = interleaveSolutionStopSpan{
+					first: planUnitsUnitFirst,
+					last:  planUnitsUnitLast,
+				}
+			}
+
+		}
+		return &data, nil
+	}
+	return nil, nil
 }
 
 func (l *interleaveConstraintImpl) SourceDisallowedInterleaves(
@@ -290,48 +376,46 @@ func (l *interleaveConstraintImpl) EstimateIsViolated(
 ) (isViolated bool, stopPositionsHint StopPositionsHint) {
 	solution := move.Solution()
 
+	moveImpl := move.(*solutionMoveStopsImpl)
+	vehicle := moveImpl.vehicle()
+	data := vehicle.last().ConstraintData(l).(*interleaveConstraintData)
+
 	solutionMoveStops := move.(*solutionMoveStopsImpl)
 
 	generator := newSolutionStopGenerator(*solutionMoveStops, true, true)
 	defer generator.release()
 
 	newPositions := make(map[SolutionStop]int)
+	oldPositions := make([]SolutionStop, 0, vehicle.NumberOfStops()+2)
 
 	position := 0
 	for solutionStop, ok := generator.next(); ok; solutionStop, ok = generator.next() {
 		newPositions[solutionStop] = position
+		if solutionStop.IsPlanned() {
+			oldPositions = append(oldPositions, solutionStop)
+		}
 		position += 1
 	}
 
 	newPlanUnitSpanFirstPosition := move.Previous().Position() + 1
 	newPlanUnitSpanLastPosition := move.Next().Position() + len(move.PlanStopsUnit().SolutionStops()) - 1
 
+	// Check if the plan unit we are moving is a plan units unit, if it is we
+	// need to check if the plan units that are already are planned of this unit
+	// spans the new positions of the plan unit we are moving, if so we are good
 	if modelPlanUnitsUnit, hasModelPlanUnitsUnit :=
 		move.PlanStopsUnit().ModelPlanUnit().PlanUnitsUnit(); hasModelPlanUnitsUnit {
-		var first, last SolutionStop
-		for _, planUnit := range modelPlanUnitsUnit.PlanUnits() {
-			if planUnit.Index() == move.PlanStopsUnit().ModelPlanUnit().Index() {
-				continue
-			}
-			solutionPlanUnit := solution.SolutionPlanUnit(planUnit)
-			if solutionPlanUnit.IsPlanned() {
-				for _, solutionPlanStopsUnit := range solutionPlanUnit.PlannedPlanStopsUnits() {
-					if solutionPlanStopsUnit.SolutionStops()[0].Vehicle() != move.Vehicle() {
-						continue
-					}
 
-					first, last = determineFirstLastSolutionStops(first, last, solutionPlanStopsUnit)
-					if first == nil {
-						continue
-					}
-				}
+		if _, ok := data.solutionPlanStopUnits[modelPlanUnitsUnit.Index()]; ok {
+			firstSolutionStop := oldPositions[data.solutionPlanStopUnits[modelPlanUnitsUnit.Index()].first]
+			if newPositions[firstSolutionStop] < newPlanUnitSpanFirstPosition {
+				newPlanUnitSpanFirstPosition = newPositions[firstSolutionStop]
 			}
-		}
-		if first != nil && newPositions[first] <= newPlanUnitSpanFirstPosition {
-			newPlanUnitSpanFirstPosition = newPositions[first]
-		}
-		if last != nil && newPositions[last] >= newPlanUnitSpanLastPosition {
-			newPlanUnitSpanLastPosition = newPositions[last]
+
+			lastSolutionStop := oldPositions[data.solutionPlanStopUnits[modelPlanUnitsUnit.Index()].last]
+			if newPositions[lastSolutionStop] > newPlanUnitSpanLastPosition {
+				newPlanUnitSpanLastPosition = newPositions[lastSolutionStop]
+			}
 		}
 	}
 
@@ -348,6 +432,7 @@ func (l *interleaveConstraintImpl) EstimateIsViolated(
 						if solutionPlanStopsUnit.SolutionStops()[0].Vehicle() != move.Vehicle() {
 							continue
 						}
+
 						sourceSpanFirst, sourceSpanLast = determineFirstLastSolutionStops(
 							sourceSpanFirst,
 							sourceSpanLast,
@@ -386,11 +471,14 @@ func (l *interleaveConstraintImpl) EstimateIsViolated(
 						targetSpanLast,
 						plannedSolutionStops,
 					)
+					newTargetSpanFirstPosition := newPositions[targetSpanFirst]
+					newTargetSpanLastPosition := newPositions[targetSpanLast]
+
 					if isViolatedPositions(
 						newPlanUnitSpanFirstPosition,
 						newPlanUnitSpanLastPosition,
-						newPositions[targetSpanFirst],
-						newPositions[targetSpanLast],
+						newTargetSpanFirstPosition,
+						newTargetSpanLastPosition,
 					) {
 						return true, noPositionsHint()
 					}
