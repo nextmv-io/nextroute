7ed9f0e46c6b7241339244cc99ca1702d84bd827
larsbeck <lars.beckmann@gmail.com>
2024-05-08 15:02:23 +0200

    chore: Improve summary section in schema.go
    This commit improves the summary section in the schema.go file by updating the field names and adding more descriptive comments. It also increases the PlanUnitsBestMoveFailed field in the Summary struct to accurately reflect when a best move fails to execute

--- check/schema/schema.go
+++ check/schema/schema.go
@@ -55,14 +55,16 @@ type Summary struct {
 	// PlanUnitsMoveFoundExecutable is the number of plan units for which at
 	// least one move has been found and the move is executable.
 	PlanUnitsBestMoveFound int `json:"plan_units_best_move_found"`
-	// PlanUnitsHaveNoMove is the number of plan units for which no feasible
-	// move has been found. This implies there is no move that can be executed
-	// without violating a constraint.
+	// PlanUnitsHaveNoMove is the number of plan units for which one of the two
+	// statements is true: 1) the constraint estimation determined that there is
+	// no move or 2) the constraint estimation determined that there is a move
+	// but when we tried to execute it, it was not executable. This implies
+	// there is no move that can be executed without violating a constraint.
 	PlanUnitsHaveNoMove int `json:"plan_units_have_no_move"`
-	// PlanUnitsMoveFoundTooExpensive is the number of plan units for which the
-	// best move is executable but would increase the objective value instead
-	// of decreasing it.
-	PlanUnitsBestMoveIncreasesObjective int `json:"plan_units_best_move_increases_objective"`
+	// NumberOfPlanUnitsMakingObjectiveWorse is the number of plan units for
+	// which the best move is executable but would increase the objective value
+	// instead of decreasing it.
+	NumberOfPlanUnitsMakingObjectiveWorse int `json:"number_of_plan_units_making_objective_worse"`
 	// PlanUnitsBestMoveFailed is the number of plan units for which the best
 	// move can not be planned. This should not happen if all the constraints
 	// are implemented correct.
@@ -86,13 +88,16 @@ type PlanUnit struct {
 	// ID is the ID of the plan unit. The ID of the plan unit is the slice of
 	// ID's of the stops in the plan unit.
 	Stops []string `json:"stops"`
-	// HasBestMove is true if a move is found for the plan unit. A plan unit
-	// has no move found if the plan unit is over-constrained or the move found
-	// is too expensive.
-	HasBestMove bool `json:"has_best_move"`
-	// BestMoveIncreasesObjective is true if the best move for the plan unit
+	// HasPlannableBestMove is true if a move is found and we were able to
+	// execute it successfully for the plan unit. A plan unit has no move found
+	// if the plan unit is over-constrained or the move found is too expensive
+	// or the constraints estimated it should be executable but when we tried to
+	// execute it, it wasn't executable - this is an error in the constraint
+	// estimation.
+	HasPlannableBestMove bool `json:"has_plannable_best_move"`
+	// PlanningMakesObjectiveWorse is true if the best move for the plan unit
 	// increases the objective.
-	BestMoveIncreasesObjective bool `json:"best_move_increases_objective"`
+	PlanningMakesObjectiveWorse bool `json:"planning_makes_objective_worse"`
 	// BestMoveFailed is true if the plan unit best move failed to execute.
 	BestMoveFailed bool `json:"best_move_failed"`
 	// VehiclesHaveMoves is the number of vehicles that have moves for the plan
@@ -100,13 +105,38 @@ type PlanUnit struct {
 	VehiclesHaveMoves *int `json:"vehicles_have_moves,omitempty"`
 	// VehicleWithMoves is the ID of the vehicles that have moves for the plan
 	// unit. Only calculated if the verbosity is very high.
-	VehiclesWithMoves *[]string `json:"vehicles_with_moves,omitempty"`
+	VehiclesWithMoves []*VehiclesWithMovesDetail `json:"vehicles_with_moves,omitempty"`
 	// BestMoveObjective is the estimate of the objective of the best move if
 	// the plan unit has a best move.
-	BestMoveObjective *Objective `json:"best_move_objective"`
-
+	BestMoveObjective *Objective `json:"best_move_objective,omitempty"`
 	// Constraints is the constraints that are violated for the plan unit.
-	Constraints *map[string]int `json:"constraints,omitempty"`
+	Constraints map[string]int `json:"constraints,omitempty"`
+}
+
+type VehiclesWithMovesDetail struct {
+	// Vehicle is the ID of the vehicle.
+	VehicleID string `json:"vehicle_id"`
+	// DeltaObjectiveEstimate is the estimate of the delta of the objective of
+	// that will be incurred by the move.
+	DeltaObjectiveEstimate *float64 `json:"delta_objective_estimate,omitempty"`
+	// DeltaObjective is the actual delta of the objective of that will be
+	// incurred by the move.
+	DeltaObjective *float64 `json:"delta_objective,omitempty"`
+	// FailedConstraints are the constraints that are violated for the move.
+	FailedConstraints []string `json:"failed_constraints,omitempty"`
+	// WasPlannable is true if the move was plannable, false otherwise.
+	WasPlannable bool `json:"was_plannable"`
+	// Positions defines where
+	Positions []Position `json:"positions"`
+}
+
+type Position struct {
+	// Previous is the ID of the stop after which the stop should be inserted.
+	Previous string `json:"previous"`
+	// Stop is the ID of the stop to be inserted.
+	Stop string `json:"stop"`
+	// Next is the ID of the stop before which the stop should be inserted.
+	Next string `json:"next"`
 }
 
 // ObjectiveTerm is the check of the individual terms of
